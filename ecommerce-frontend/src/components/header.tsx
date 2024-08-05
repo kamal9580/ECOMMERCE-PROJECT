38c3eca65ecf497a762b925455dccb6db6c40506
kamal9580 <nileshprince3118@gmail.com>
2024-08-05 23:29:07 +0530

    fix signout problem

--- ecommerce-frontend/src/components/header.tsx
+++ ecommerce-frontend/src/components/header.tsx
@@ -2,6 +2,9 @@ import { useState } from "react";
 import { FaSearch, FaShoppingBag, FaSignInAlt, FaSignOutAlt, FaUser } from "react-icons/fa"
 import { Link } from "react-router-dom"
 import { User } from "../types/types";
+import { signOut } from "firebase/auth";
+import toast from "react-hot-toast";
+import { auth } from "../firebase";
 
 interface PropsType {
   user: User | null;
@@ -11,9 +14,14 @@ const Header = ({user}:  PropsType) => {
 
   const [isOpen,setIsOpen] =useState<boolean>(false);
 
-  const logoutHandler =()=> {
-
-     setIsOpen(false);
+  const logoutHandler = async () => {
+    try {
+      await signOut(auth);
+      toast.success("Sign Out Successfully");
+      setIsOpen(false);
+    } catch (error) {
+      toast.error("Sign Out Fail");
+    }
   };
 
   return (
